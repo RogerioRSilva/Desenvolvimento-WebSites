299055dd2dd8bf2a17ad76790db69fa92bc1978e
RogerioRSilva <rogrodriguessilva2022@gmail.com>
2024-07-09 22:57:45 -0300

    Ajustes na home e modificação do layout

--- site-paralax/js/script.js
+++ site-paralax/js/script.js
@@ -1,8 +1,8 @@
-const btn_card_01 = document.querySelector('button');
+const btn_card_01 = document.getElementById('btn_card_01');
 
-const modal_card_01 = document.querySelector('dialog');
+const modal_card_01 = document.getElementById('dialog_01');
 
-const btn_close_card_01 = document.querySelector('dialog button');
+const btn_close_card_01 = document.getElementById('btn_close_dialog_01');
 
 btn_card_01.onclick = function () {
 	modal_card_01.showModal();
@@ -39,4 +39,47 @@ btn_card_03.onclick = function () {
 btn_close_card_03.onclick = function () {
 	modal_card_03.close();
 }
+
+const btn_card_04 = document.getElementById('btn_card_04');
+
+const modal_card_04 = document.getElementById('dialog_04');
+
+const btn_close_card_04 = document.getElementById('btn_close_dialog_04');
+
+btn_card_04.onclick = function () {
+	modal_card_04.showModal();
+}
+
+btn_close_card_04.onclick = function () {
+	modal_card_04.close();
+}
+
+const btn_card_05 = document.getElementById('btn_card_05');
+
+const modal_card_05 = document.getElementById('dialog_05');
+
+const btn_close_card_05 = document.getElementById('btn_close_dialog_05');
+
+btn_card_05.onclick = function () {
+	modal_card_05.showModal();
+}
+
+btn_close_card_05.onclick = function () {
+	modal_card_05.close();
+}
+
+const btn_card_06 = document.getElementById('btn_card_06');
+
+const modal_card_06 = document.getElementById('dialog_06');
+
+const btn_close_card_06 = document.getElementById('btn_close_dialog_06');
+
+btn_card_06.onclick = function () {
+	modal_card_06.showModal();
+}
+
+btn_close_card_06.onclick = function () {
+	modal_card_06.close();
+}
+
 
